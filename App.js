f9da1f689b5d017c33b21b242f63714e648dfd28
Babasanya <Babz@Babz-Macbook-Air.local>
2017-08-03 01:33:40 +0100

    Add firebase to project

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/components/common'
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCUhxgDl3WP-40PXpXyupcCow5wBIt70iY",
+      authDomain: "react-native-auth-ee850.firebaseapp.com",
+      databaseURL: "https://react-native-auth-ee850.firebaseio.com",
+      projectId: "react-native-auth-ee850",
+      storageBucket: "react-native-auth-ee850.appspot.com",
+      messagingSenderId: "551640282962"
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Header headerText="Authentication" />
+        <Text>Rest of app</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
